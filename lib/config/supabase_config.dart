018ee68ed89bd9ffae46023899236341e6580a0e
István Eckert <eckert.isti@gmail.com>
2025-10-02 01:53:44 +0200

    Update Google OAuth client IDs in Supabase configuration

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -3,6 +3,6 @@ class SupabaseConfig {
   static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0cWhlcm12cnZ5Y3NwdWNnenRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyNTc3MjYsImV4cCI6MjA3NDgzMzcyNn0.WldxvPwACjSZKfxlXBsgIa9jQB8CC9bAcV8kW-gHW-k';
   
   // Google OAuth konfiguráció
-  static const String googleClientId = '529923259303-6ac2151j0an5dur0j37976679vj647q3.apps.googleusercontent.com'; // Web client
-  static const String googleAndroidClientId = '529923259303-hshkgvsm4o21u4r7b9r83gkikr5tek0t.apps.googleusercontent.com'; // Android client
+  static const String googleClientId = '1020627864569-q2iuillmfb43d5o3pt1ao1mt01o018it.apps.googleusercontent.com'; // Web client
+  static const String googleAndroidClientId = '1020627864569-04me5rput5g93n66v30u6tvmmk7idvgt.apps.googleusercontent.com'; // Android client
 }
